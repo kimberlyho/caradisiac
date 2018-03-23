a02558d58195a793dfc122bd36ae1a6d4a6bc1c6
kimberlyho <kimberlyho@hotmail.fr>
2018-03-23 16:49:21 +0100

    feat(api): api created to populate data in elastic search and sort the list of suv in descending order

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const {getModels} = require('node-car-api');
 var fs = require('fs');
 var jsonfile = require('jsonfile');
 var elasticsearch = require('elasticsearch');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
-///////////////////////////////////// GET DATA FROM CARADISIAC ///////////////////////////////
+
 async function getBrand(){
   const brands = await getBrands();
   return brands;
@@ -30,32 +33,72 @@ brands.then(function(result){
   }
 });
 
-
-//////////////////////////////////// INDEX ALL RECORDS TO ELASTICSEARCH ///////////////////////////////
-
-var file = "./caradisiac.json";
-var caradisiac = jsonfile.readFileSync(file);
-
-var client = new elasticsearch.Client({
-    host: 'localhost:9200',
-    log: 'trace'
+const port = 9292;
+app.listen(port, () => {
+  console.log('We are live on ' + port);
 });
 
-var body = [];
-for (var i = 0; i < caradisiac.length; i++ ) {
-    var config = { index:  { _index: 'caradisiac', _type: 'suv', _id: i } };
-    body.push(config);
-    body.push(caradisiac[i]);
-}
+app.get('/populate',function(req,res){
 
-client.bulk({
-    body: body
-}, function (error, response) {
-    if (error) {
-        console.error(error);
-        return;
+  var file = "./caradisiac.json";
+  var caradisiac = jsonfile.readFileSync(file);
+
+  var client = new elasticsearch.Client({
+      host: 'localhost:9200',
+      log: 'trace'
+  });
+
+  var body = [];
+  for (var i = 0; i < caradisiac.length; i++ ) {
+      var config = { index:  { _index: 'caradisiac', _type: 'suv', _id: i } };
+      body.push(config);
+      body.push(caradisiac[i]);
+  }
+
+  client.bulk({
+      body: body
+  }, function (error, response) {
+      if (error) {
+          console.error(error);
+          return;
+      }
+      else {
+          console.log(response);
+      }
+  });
+  res.send("hello");
+})
+
+
+app.get('/suv', function(req,res){
+
+  var file = "./caradisiac.json";
+  var caradisiac = jsonfile.readFileSync(file);
+
+  var client = new elasticsearch.Client({
+      host: 'localhost:9200',
+      log: 'trace'
+  });
+
+  client.search({
+    index: 'caradisiac',
+    type: 'suv',
+    body:{
+      "size":80,
+      "sort":[
+        {"volume.keyword" :{"order":"desc"}}
+      ]
+    }
+  },function (error, response,status) {
+    if (error){
+      console.log("search error: "+error)
     }
     else {
-        console.log(response);
+      console.log("--- Response ---");
+      console.log(response);
+      console.log("--- Hits ---");
+      res.send(response.hits.hits);
     }
-});
+  });
+
+})
